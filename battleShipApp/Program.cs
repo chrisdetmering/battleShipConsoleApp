958d1b628e5bee945f2c3699b6713568940db9be
froggy <chrisdetmering@gmail.com>
2021-04-13 15:01:46 -0700

    initial commit

---
+++ battleShipApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace battleShipApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
